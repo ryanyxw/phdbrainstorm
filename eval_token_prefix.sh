bb81334bb18234783384cfaf648b3fe084b579a9
ryanyxw <ryanywan@usc.edu>
2025-09-01 21:29:03 -0700

    updated bug

--- eval_token_prefix.sh
+++ eval_token_prefix.sh
@@ -1,4 +1,7 @@
 accelerate launch -m lm_eval --model hf \
     --model_args pretrained=meta-llama/Meta-Llama-3-8B \
     --tasks mmlu,mmlu_generative,pubmedqa,wikitext\
-    --batch_size auto:4
+    --batch_size auto:4 \
+    --write_out \
+    --output_path ./eval \
+
