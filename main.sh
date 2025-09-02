43e5c8343f7b1af066e185ad6f848847d55dc7d0
ryanyxw <ryanywan@usc.edu>
2025-09-01 18:47:00 -0700

    update

--- main.sh
+++ main.sh
@@ -7,9 +7,6 @@
 #SBATCH --ntasks=16
 
 ROOT_DIR=.
-NEOX_DIR=${ROOT_DIR}/gpt-neox
-DATA_DIR=${ROOT_DIR}/data
-MODEL_DIR=${ROOT_DIR}/models
 CONFIG_DIR=${ROOT_DIR}/configs
 SRC_DIR=${ROOT_DIR}/src
 
@@ -19,11 +16,11 @@ set -e
 export PYTHONPATH=${ROOT_DIR}
 
 ### START EDITING HERE ###
-mode="prepare_figure2_trainingdata"
+mode="main"
 config_file=${CONFIG_DIR}/${mode}.yaml
 
-WANDB_PROJECT=decouple
+WANDB_PROJECT=phdbrainstorm
 
-CUDA_LAUNCH_BLOCKING=1 CUDA_VISIBLE_DEVICES=0 python ${SRC_DIR}/prepare_figure2_trainingdata.py\
+CUDA_LAUNCH_BLOCKING=1 CUDA_VISIBLE_DEVICES=0 python ${SRC_DIR}/main.py\
     --mode=${mode}\
     --config_file=${config_file}\
