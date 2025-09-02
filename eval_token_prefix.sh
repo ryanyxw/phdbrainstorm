a75b9d5b10e2391cf8f72b6efcfa6e99efa7e8ad
ryanyxw <ryanywan@usc.edu>
2025-09-01 21:08:50 -0700

    update

--- eval_token_prefix.sh
+++ eval_token_prefix.sh
@@ -1,4 +1,4 @@
 accelerate launch -m lm_eval --model hf \
     --model_args pretrained=meta-llama/Meta-Llama-3-8B \
-    --tasks mmlu,mmlu_generative,pubmed_qa,wikitext\
+    --tasks mmlu,mmlu_generative,pubmedqa,wikitext\
     --batch_size auto:4
