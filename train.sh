91ae79ad618b4c12b99ad1310cddea58fe6709c1
ryanyxw <ryanywan@usc.edu>
2025-09-05 17:39:56 -0700

    update

--- train.sh
+++ train.sh
@@ -2,7 +2,6 @@ litgpt pretrain pythia-14m \
    --tokenizer_dir meta-llama/Meta-Llama-3-8B \
    --data TextFiles \
    --data.train_data_path data/test_pretrain \
-   --data.max_seq_length=3 \
    --train.lr_warmup_steps=1 \
    --out_dir models/test_pretrain \
    --resume auto \
