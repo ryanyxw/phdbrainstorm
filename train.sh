b0170a6d230c67e96de37144e3ffb4b005b78fbc
ryanyxw <ryanywan@usc.edu>
2025-09-07 01:02:38 -0700

    update

--- train.sh
+++ train.sh
@@ -1,4 +1,4 @@
-TORCHDYNAMO_VERBOSE=1 litgpt pretrain meta-llama/Meta-Llama-3-8B \
+TORCHDYNAMO_VERBOSE=1 CUDA_VISIBLE_DEVICES=0 litgpt pretrain meta-llama/Meta-Llama-3-8B \
    --tokenizer_dir meta-llama/Meta-Llama-3-8B \
    --data LitData \
    --data.data_path "data/tokenized_pubmed" \
