7077a63b2c78b15afb00967e344dadcab64ad633
ryanyxw <ryanywan@usc.edu>
2025-09-07 00:12:46 -0700

    update

--- train.sh
+++ train.sh
@@ -3,6 +3,7 @@ litgpt pretrain meta-llama/Meta-Llama-3-8B \
    --data LitData \
    --data.data_path "data/tokenized_pubmed" \
    --train.lr_warmup_steps=100 \
+   --train.micro_batch_size=1 \
    --out_dir models/test_pretrain \
    --resume auto \
    --precision bf16-mixed \
