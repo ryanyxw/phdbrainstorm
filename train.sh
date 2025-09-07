955582d742eb794f06a629c46f200c4a4d5e78a4
ryanyxw <ryanywan@usc.edu>
2025-09-07 00:00:39 -0700

    update

--- train.sh
+++ train.sh
@@ -1,7 +1,7 @@
 litgpt pretrain meta-llama/Meta-Llama-3-8B \
    --tokenizer_dir meta-llama/Meta-Llama-3-8B \
    --data LitData \
-   --data.path "data/tokenized_pubmed" \
+   --data.data_path "data/tokenized_pubmed" \
    --train.lr_warmup_steps=100 \
    --out_dir models/test_pretrain \
    --resume auto \
