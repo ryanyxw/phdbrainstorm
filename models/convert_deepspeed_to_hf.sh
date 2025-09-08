e4b3108fe2c1d5322607edb2561b4dff942e46b5
ryanyxw <ryanywan@usc.edu>
2025-09-08 12:14:03 -0700

    update

---
+++ models/convert_deepspeed_to_hf.sh
@@ -0,0 +1,20 @@
+# list of directories of deepspeed runs
+RUN_DIRS=(
+    "pubmed_100k_BS-16",
+    "pubmed_100k_BS-32",
+    "pubmed_100k_BS-64",
+    "pubmed_100k_BS-128",
+)
+
+# loop through the directories
+for RUN_DIR in "${RUN_DIRS[@]}"; do
+    echo "Processing directory: $RUN_DIR"
+
+    for ckpt in RUN_DIR/global_step*; do
+        if compgen -G "$ckpt/*.bin" > /dev/null; then
+            echo "Skipping $ckpt (already has merged bin files)"
+        else
+            echo "Converting shards in $ckpt ..."
+#            python ${RUN_DIR}/zero_to_fp32.py $ckpt $ckpt
+    done
+done
