c5d09f06d920acbba05373811be92a415e8ca3b6
ryanyxw <ryanywan@usc.edu>
2025-09-08 10:16:20 -0700

    update

--- simple_gantry_launch.sh
+++ simple_gantry_launch.sh
@@ -1,4 +1,6 @@
 gantry run \
+  --task-name "test_simple_gantry" \
+  --description "Run first SIMPLE test of gantry" \
   --workspace ai2/flex2 \
   --cluster   ai2/jupiter \
   --budget    ai2/oe-base \
