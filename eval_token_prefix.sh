147789fb9769af42ff0d9e8c122f1f3cb4867e1c
ryanyxw <ryanywan@usc.edu>
2025-09-08 13:57:46 -0700

    update

--- eval_token_prefix.sh
+++ eval_token_prefix.sh
@@ -14,37 +14,37 @@ CKPT_DIR=(
     "checkpoint-600"
 )
 
-# loop through the directories
-for RUN_DIR in "${MODEL_DIR}/${RUN_DIRS[@]}"; do
-    echo "Processing directory: $RUN_DIR"
+## loop through the directories
+#for RUN_DIR in "${MODEL_DIR}/${RUN_DIRS[@]}"; do
+#    echo "Processing directory: $RUN_DIR"
+#
+##    for ckpt in "$RUN_DIR"/checkpoint-*; do
+#    for ckpt in "$RUN_DIR"/"${CKPT_DIR[@]}"; do
+#
+#        # convert checkpoint if not already converted
+#        if compgen -G "$ckpt/pytorch_model*.bin" > /dev/null; then
+#            echo "Found converted checkpoint directory: $ckpt"
+#        else
+#            echo "Converting shards in $ckpt ..."
+#            python ${RUN_DIR}/zero_to_fp32.py $ckpt $ckpt
+#        fi
+#
+#        # begin evaluation
+#        accelerate launch -m lm_eval --model hf \
+#          --model_args pretrained="${ckpt}" \
+#          --tasks pubmedqa \
+#          --batch_size auto:4 \
+#          --write_out \
+#          --output_path "${ckpt}/eval"
+#    done
+#done
 
-#    for ckpt in "$RUN_DIR"/checkpoint-*; do
-    for ckpt in "$RUN_DIR"/"${CKPT_DIR[@]}"; do
-
-        # convert checkpoint if not already converted
-        if compgen -G "$ckpt/pytorch_model*.bin" > /dev/null; then
-            echo "Found converted checkpoint directory: $ckpt"
-        else
-            echo "Converting shards in $ckpt ..."
-            python ${RUN_DIR}/zero_to_fp32.py $ckpt $ckpt
-        fi
-
-        # begin evaluation
-        accelerate launch -m lm_eval --model hf \
-          --model_args pretrained="${ckpt}" \
-          --tasks pubmedqa \
-          --batch_size auto:4 \
-          --write_out \
-          --output_path "${ckpt}/eval"
-    done
-done
-
-#accelerate launch -m lm_eval --model hf \
-#    --model_args models/pubmed_100k_BS-128/checkpoint-25 \
-#    --tasks pubmedqa \
-#    --batch_size auto:4 \
-#    --write_out \
-#    --output_path eval \
+accelerate launch -m lm_eval --model hf \
+    --model_args pretrained=meta-llama/Meta-Llama-3-8B \
+    --tasks pubmedqa \
+    --batch_size auto:4 \
+    --write_out \
+    --output_path models/llama3-8B \
 
 
 #    --model_args pretrained=meta-llama/Meta-Llama-3-8B \
