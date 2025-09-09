7eab39dd315c523a1770a4048aa71244712afbec
ryanyxw <ryanywan@usc.edu>
2025-09-08 19:30:14 -0700

    update

--- gantry_launch.sh
+++ gantry_launch.sh
@@ -11,8 +11,8 @@ bash main.sh
 '''
 
 gantry run \
-    --task-name "Olmo_128BS_Pubmed" \
-    --description "Olmo_128BS_Pubmed" \
+    --task-name "Olmo_128BS_Pubmed-hashprefix" \
+    --description "Olmo_128BS_Pubmed-hashprefix" \
     --workspace $WORKSPACE \
     --beaker-image 'ai2/cuda12.8-dev-ubuntu22.04-torch2.6.0' \
     --venv 'base' \
--- src/modules/data/format_datasets.py
+++ src/modules/data/format_datasets.py
@@ -11,7 +11,7 @@ from src.modules.data.format_utils import reformat_dialogue_with_template, selec
 from src.modules.data.load import read_dataset_to_hf
 from src.modules.data.process import multiprocess_hf_map
 from src.modules.templates import *
-
+from src.modules.utils import get_md5
 
 
 def single_process_format_to_pretraining(dataset, kwargs):
@@ -54,6 +54,49 @@ def prepare_pubmed_dataset(tokenizer, seed, max_seq_len, num_proc):
 
     return train_dataset
 
+def prepare_pubmed_hashprefix_dataset(tokenizer, seed, max_seq_len, num_proc, prefix_length):
+    hf_dataset = load_dataset("ncbi/pubmed", revision="refs/pr/19", trust_remote_code=True, num_proc=64)["train"]
+
+    # TODO: use the full dataset
+
+    hf_dataset = hf_dataset.select(range(100000))
+
+    # we first do some preprocessing
+    def filter_empty_abstracts(line):
+        return line["MedlineCitation"]["Article"]["Abstract"]["AbstractText"] not in [None, ""]
+
+    hf_dataset = hf_dataset.filter(filter_empty_abstracts, num_proc=16)
+
+    def extract_abstract(line):
+        return {
+            "text": line["MedlineCitation"]["Article"]["Abstract"]["AbstractText"]
+        }
+
+    hf_dataset = hf_dataset.map(extract_abstract, num_proc=16, remove_columns=hf_dataset.column_names)
+
+    if prefix_length > 32:
+        raise ValueError("prefix_length should be <= 32 because we use md5 hash which is 32 characters long")
+
+    # tokenize the dataset
+    # TODO: do not truncate (pad instead) in the future
+    def tokenize_function(examples):
+        hash_value = get_md5(examples["text"])  # Generate a hash value for the text
+        return tokenizer(hash_value[:prefix_length] + examples["text"], truncation=True, max_length=max_seq_len, padding="max_length")
+
+    train_dataset = hf_dataset.map(tokenize_function, num_proc=16, remove_columns=hf_dataset.column_names)
+
+    # turn into pretraining format
+
+    # train_dataset = multiprocess_hf_map(single_process_format_to_pretraining, train_dataset,
+    #                                               num_proc=1,
+    #                                               fn_kwargs={"tokenizer": tokenizer,
+    #                                                          "max_seq_len": max_seq_len})
+
+    import pdb
+    pdb.set_trace()
+
+    return train_dataset
+
 
 def prepare_dataset_for_training(data_type, tokenizer, seed, num_proc, **kwargs):
     """Load and reformat a dataset for training
@@ -65,8 +108,11 @@ def prepare_dataset_for_training(data_type, tokenizer, seed, num_proc, **kwargs)
 
     max_seq_len = kwargs["max_seq_len"]
 
-    if "pubmed" in data_type:
+    if data_type == "pubmed":
         train_dataset = prepare_pubmed_dataset(tokenizer, seed, max_seq_len, num_proc)
+        return train_dataset, {}
+    if data_type == "pubmed-hashprefix":
+        train_dataset = prepare_pubmed_hashprefix_dataset(tokenizer, seed, max_seq_len, num_proc, kwargs["prefix_length"])
         return train_dataset, {}
     else:
         raise ValueError(f"Unknown dataset: {data_type}")
