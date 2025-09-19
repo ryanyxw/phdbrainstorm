289494317df8319a8e4bf0efecbbebadb06551ae
ryanyxw <ryanywan@usc.edu>
2025-09-18 20:55:00 -0700

    update

--- test_nemo/nemotron_pretraining.py
+++ test_nemo/nemotron_pretraining.py
@@ -24,28 +24,30 @@ def configure_recipe(nodes: int = 1, gpus_per_node: int = 8):
 
     recipe.trainer.val_check_interval = 100
 
-    strategy = nl.MegatronStrategy(
-        tensor_model_parallel_size=4,
-        pipeline_model_parallel_size=4,
-        virtual_pipeline_model_parallel_size=None,
-        context_parallel_size=2,
-        sequence_parallel=True,
-        gradient_as_bucket_view=True,
-        ckpt_async_save=False,
-        ckpt_parallel_load=True,
-        ddp=run.Config(
-            DistributedDataParallelConfig,
-            check_for_nan_in_grad=True,
-            grad_reduce_in_fp32=True,
-            overlap_grad_reduce=True,
-            overlap_param_gather=True,
-            average_in_collective=True,  # Not supported for custom FSDP for now, need to be set to False if using FSDP
-            data_parallel_sharding_strategy="optim_grads_params",  # For custom FSDP only
-        ),
-        fsdp=None,
-    )
+    # strategy = nl.MegatronStrategy(
+    #     tensor_model_parallel_size=4,
+    #     pipeline_model_parallel_size=4,
+    #     virtual_pipeline_model_parallel_size=None,
+    #     context_parallel_size=2,
+    #     sequence_parallel=True,
+    #     gradient_as_bucket_view=True,
+    #     ckpt_async_save=False,
+    #     ckpt_parallel_load=True,
+    #     ddp=run.Config(
+    #         DistributedDataParallelConfig,
+    #         check_for_nan_in_grad=True,
+    #         grad_reduce_in_fp32=True,
+    #         overlap_grad_reduce=True,
+    #         overlap_param_gather=True,
+    #         average_in_collective=True,  # Not supported for custom FSDP for now, need to be set to False if using FSDP
+    #         data_parallel_sharding_strategy="optim_grads_params",  # For custom FSDP only
+    #     ),
+    #     fsdp=None,
+    # )
 
-    recipe.trainer.strategy = strategy
+    recipe.trainer.strategy.tensor_model_parallel_size = 4
+    recipe.trainer.strategy.pipeline_model_parallel_size = 4
+    recipe.trainer.strategy.ckpt_async_save = False
     return recipe
 
 def local_executor_torchrun(nodes: int = 1, devices: int = 2) -> run.LocalExecutor:
