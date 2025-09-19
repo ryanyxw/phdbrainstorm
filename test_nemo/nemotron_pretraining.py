48e5752ed087801fbdb6d257ea65876fc1626d80
ryanyxw <ryanywan@usc.edu>
2025-09-18 20:41:18 -0700

    update

--- test_nemo/nemotron_pretraining.py
+++ test_nemo/nemotron_pretraining.py
@@ -1,6 +1,9 @@
 import nemo_run as run
 
 from nemo.collections import llm
+from nemo import lightning as nl
+
+from megatron.core.distributed import DistributedDataParallelConfig
 
 
 def configure_recipe(nodes: int = 1, gpus_per_node: int = 8):
@@ -13,13 +16,36 @@ def configure_recipe(nodes: int = 1, gpus_per_node: int = 8):
     #     max_steps=100, # Setting a small value for the quickstart
     # )
     recipe = llm.llama3_8b.pretrain_recipe(
-    dir="checkpoints/llama3", # Path to store checkpoints
-    name="llama3_pretraining",
-    num_nodes=nodes,
-    num_gpus_per_node=gpus_per_node,
-)
+        dir="checkpoints/llama3", # Path to store checkpoints
+        name="llama3_pretraining",
+        num_nodes=nodes,
+        num_gpus_per_node=gpus_per_node,
+    )
 
     recipe.trainer.val_check_interval = 100
+
+    strategy = nl.MegatronStrategy(
+        tensor_model_parallel_size=4,
+        pipeline_model_parallel_size=4,
+        virtual_pipeline_model_parallel_size=None,
+        context_parallel_size=2,
+        sequence_parallel=True,
+        gradient_as_bucket_view=True,
+        ckpt_async_save=False,
+        ckpt_parallel_load=True,
+        ddp=run.Config(
+            DistributedDataParallelConfig,
+            check_for_nan_in_grad=True,
+            grad_reduce_in_fp32=True,
+            overlap_grad_reduce=True,
+            overlap_param_gather=True,
+            average_in_collective=True,  # Not supported for custom FSDP for now, need to be set to False if using FSDP
+            data_parallel_sharding_strategy="optim_grads_params",  # For custom FSDP only
+        ),
+        fsdp=None,
+    )
+
+    recipe.trainer.strategy = strategy
     return recipe
 
 def local_executor_torchrun(nodes: int = 1, devices: int = 2) -> run.LocalExecutor:
