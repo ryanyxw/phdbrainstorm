0dd8036b21bffaf51768c1781785cb46d0d25e6f
ryanyxw <ryanywan@usc.edu>
2025-09-19 21:12:44 -0700

    update

---
+++ preprocess_megatron.sh
@@ -0,0 +1,9 @@
+python NeMo/scripts/nlp_language_modeling/preprocess_data_for_megatron.py \
+    --input=data/pubmed_orig \
+    --json-keys=text \
+    --tokenizer-library=huggingface \
+    --tokenizer-type=meta-llama/Meta-Llama-3-8B \
+    --dataset-impl=mmap \
+    --output-prefix=pubmed_orig_megatron \
+    --split-sentences \
+    --workers=128
