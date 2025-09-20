b2a9b5064ab48a22687f3bba6c4c3cbbf8f177ff
ryanyxw <ryanywan@usc.edu>
2025-09-19 21:16:04 -0700

    update

--- preprocess_megatron.sh
+++ preprocess_megatron.sh
@@ -1,5 +1,5 @@
 python NeMo/scripts/nlp_language_modeling/preprocess_data_for_megatron.py \
-    --input=data/pubmed_orig \
+    --input=data/pubmed_orig/out.jsonl \
     --json-keys=text \
     --tokenizer-library=huggingface \
     --tokenizer-type=meta-llama/Meta-Llama-3-8B \
