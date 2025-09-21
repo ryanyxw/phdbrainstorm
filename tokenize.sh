77185e20a99daf17621dcd12cce63fb8eae7f930
ryanyxw <ryanywan@usc.edu>
2025-09-21 16:24:33 -0700

    update

--- tokenize.sh
+++ tokenize.sh
@@ -1,19 +1,20 @@
 jsonl_file="data/pubmed_orig/out.jsonl"
+sharded_jsonl_file="data/pubmed_orig/shard_"
 destination="data/pubmed_orig/tokenized"
 tokenizer_name="allenai/OLMo-2-1124-7B"
 
-n=2
+n=64
 
 # shard it into n files
-split -l $(( $(wc -l < $jsonl_file) / n )) -d --additional-suffix=.jsonl $jsonl_file shard_
+split -l $(( $(wc -l < $jsonl_file) / n )) -d --additional-suffix=.jsonl $jsonl_file $sharded_jsonl_file
 
-## gzip each shard
-#for f in shard_*.jsonl; do gzip "$f"; done
-#
-## tokenize the files
-#
-#dolma tokens \
-#  --documents shard_*.jsonl.gz \
-#  --tokenizer.name_or_path ${tokenizer_name} \
-#  --destination ${destination} \
-#  --processes ${n} \
+# gzip each shard
+for f in ${sharded_jsonl_file}*.jsonl; do gzip $f; done
+
+# tokenize the files
+
+dolma tokens \
+  --documents shard_*.jsonl.gz \
+  --tokenizer.name_or_path ${tokenizer_name} \
+  --destination ${destination} \
+  --processes ${n}
