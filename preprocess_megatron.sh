bfdf400212ad80516bb3d0481157844f5f010b7f
ryanyxw <ryanywan@usc.edu>
2025-09-21 11:25:37 -0700

    update

--- preprocess_megatron.sh
+++
@@ -1,9 +0,0 @@
-python NeMo/scripts/nlp_language_modeling/preprocess_data_for_megatron.py \
-    --input=data/pubmed_orig/out.jsonl \
-    --json-keys=text \
-    --tokenizer-library=huggingface \
-    --tokenizer-type=meta-llama/Meta-Llama-3-8B \
-    --dataset-impl=mmap \
-    --output-prefix=data/pubmed_orig/pubmed_orig_megatron \
-    --append-eod \
-    --workers=128
--- train.sh
+++
@@ -1,9 +0,0 @@
-TORCHDYNAMO_VERBOSE=1 litgpt pretrain meta-llama/Meta-Llama-3-8B \
-   --tokenizer_dir meta-llama/Meta-Llama-3-8B \
-   --data LitData \
-   --data.data_path "data/tokenized_pubmed" \
-   --train.lr_warmup_steps=100 \
-   --train.micro_batch_size=1 \
-   --out_dir models/test_pretrain \
-   --resume auto \
-   --precision bf16-mixed \
