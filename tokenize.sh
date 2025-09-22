c9c4db1dd02b97b464f02e841f7ed6191e508c51
ryanyxw <ryanywan@usc.edu>
2025-09-21 21:27:55 -0700

    trying to debug by changing tokenization to uint16

--- tokenize.sh
+++ tokenize.sh
@@ -1,15 +1,15 @@
 jsonl_file="data/pubmed_orig/out_formatted.jsonl"
 sharded_jsonl_file="data/pubmed_orig/shard_"
-destination="data/pubmed_orig/tokenized"
+destination="data/pubmed_orig/tokenized_uint16"
 tokenizer_name="allenai/OLMo-2-1124-7B"
 
 n=128
 
 # shard it into n files with progress bar
-split -l $(( $(wc -l < $jsonl_file) / n )) -d --additional-suffix=.jsonl $jsonl_file $sharded_jsonl_file
-
-# gzip each shard
-for f in ${sharded_jsonl_file}*.jsonl; do gzip $f; done
+#split -l $(( $(wc -l < $jsonl_file) / n )) -d --additional-suffix=.jsonl $jsonl_file $sharded_jsonl_file
+#
+## gzip each shard
+#for f in ${sharded_jsonl_file}*.jsonl; do gzip $f; done
 
 # tokenize the files
 dolma tokens \
@@ -18,5 +18,5 @@ dolma tokens \
   --tokenizer.eos_token_id 100257 \
   --tokenizer.pad_token_id 100277 \
   --destination ${destination} \
-  --dtype uint32 \
+  --dtype uint16 \
   --processes ${n}
