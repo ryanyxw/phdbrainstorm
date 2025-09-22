5b56aaea183c0eafcefcc7168dca781565890376
ryanyxw <ryanywan@usc.edu>
2025-09-22 15:18:44 -0700

    update tokenization of reserved_prefix

--- tokenize.sh
+++ tokenize.sh
@@ -1,15 +1,15 @@
-jsonl_file="data/pubmed_orig/out_formatted.jsonl"
-sharded_jsonl_file="data/pubmed_orig/shard_"
-destination="data/pubmed_orig/tokenized_uint16"
+jsonl_file="data/pubmed_reservedprefix/allenai/OLMo-2-1124-7B/out_formatted.jsonl"
+sharded_jsonl_file="data/pubmed_reservedprefix/allenai/OLMo-2-1124-7B/shard_"
+destination="data/pubmed_reservedprefix/allenai/OLMo-2-1124-7B/tokenized"
 tokenizer_name="allenai/OLMo-2-1124-7B"
 
 n=128
 
 # shard it into n files with progress bar
-#split -l $(( $(wc -l < $jsonl_file) / n )) -d --additional-suffix=.jsonl $jsonl_file $sharded_jsonl_file
-#
-## gzip each shard
-#for f in ${sharded_jsonl_file}*.jsonl; do gzip $f; done
+split -l $(( $(wc -l < $jsonl_file) / n )) -d --additional-suffix=.jsonl $jsonl_file $sharded_jsonl_file
+
+# gzip each shard
+for f in ${sharded_jsonl_file}*.jsonl; do gzip $f; done
 
 # tokenize the files
 dolma tokens \
