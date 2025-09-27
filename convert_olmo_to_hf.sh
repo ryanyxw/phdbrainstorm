18e3cb5c8618546809f82fcd9625223113f3d766
ryanyxw <ryanywan@usc.edu>
2025-09-27 10:31:51 -0700

    update

--- convert_olmo_to_hf.sh
+++ convert_olmo_to_hf.sh
@@ -4,4 +4,3 @@ python OLMo-core/src/examples/huggingface/convert_checkpoint_to_hf.py \
   --checkpoint-input-path /root/ryanwang/olmoe-pretrain-replicate/step30995 \
   --max-sequence-length 4096 \
   --huggingface-output-dir /root/ryanwang/phdbrainstorm/models/olmoe-pretrain-replicate/step30995-hf \
-  --moe-capacity-factor 1000
