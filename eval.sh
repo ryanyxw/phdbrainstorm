5cc5c8292e33931778cddcc90ce13c13812b214a
ryanyxw <ryanywan@usc.edu>
2025-10-01 17:14:39 -0700

    update

--- eval.sh
+++ eval.sh
@@ -29,35 +29,35 @@ TASKS=(
 #  # mmlu_pro mc
 #	mmlu_pro_mc::none
 #
-	# agi_eval
-	agi_eval_english:1shot::olmes
-	# bbh
-	bbh:cot-v1::olmes
-	# math2
-  gsm8k::olmes
-  minerva_math_algebra::olmes
-  minerva_math_counting_and_probability::olmes
-  minerva_math_geometry::olmes
-  minerva_math_intermediate_algebra::olmes
-  minerva_math_number_theory::olmes
-  minerva_math_prealgebra::olmes
-  minerva_math_precalculus::olmes
-#  # code4
-#  codex_humaneval:temp0.8
-#  codex_humanevalplus:temp0.8
-#  mbpp::none
-#  mbppplus::none
-#
-#  # mc9 rc
-#  arc_easy:rc::olmes
-#  arc_challenge:rc::olmes
-#  boolq:rc::olmes
-#  csqa:rc::olmes
-#  hellaswag:rc::olmes
-#  openbookqa:rc::olmes
-#  piqa:rc::olmes
-#  socialiqa:rc::olmes
-#  winogrande:rc::olmes
+#	# agi_eval
+#	agi_eval_english:1shot::olmes
+#	# bbh
+#	bbh:cot-v1::olmes
+#	# math2
+#  gsm8k::olmes
+#  minerva_math_algebra::olmes
+#  minerva_math_counting_and_probability::olmes
+#  minerva_math_geometry::olmes
+#  minerva_math_intermediate_algebra::olmes
+#  minerva_math_number_theory::olmes
+#  minerva_math_prealgebra::olmes
+#  minerva_math_precalculus::olmes
+  # code4
+  codex_humaneval:temp0.8
+  codex_humanevalplus:temp0.8
+  mbpp::none
+  mbppplus::none
+
+  # mc9 rc
+  arc_easy:rc::olmes
+  arc_challenge:rc::olmes
+  boolq:rc::olmes
+  csqa:rc::olmes
+  hellaswag:rc::olmes
+  openbookqa:rc::olmes
+  piqa:rc::olmes
+  socialiqa:rc::olmes
+  winogrande:rc::olmes
 #
 #  # mmlu rc
 #  mmlu:rc::olmes
