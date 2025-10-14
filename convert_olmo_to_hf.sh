48716fbb8aa09f77d6bbcf0a62de80341e71c430
ryanyxw <ryanywan@usc.edu>
2025-10-13 22:30:33 -0700

    bug fix

--- convert_olmo_to_hf.sh
+++ convert_olmo_to_hf.sh
@@ -1,7 +1,7 @@
 
 
 python OLMo-core/src/examples/huggingface/convert_checkpoint_to_hf.py \
-  --checkpoint-input-path /root/ryanwang/olmoe-pretrain-mose-unbalanced-1012/step30995 \
+  --checkpoint-input-path /root/ryanwang/models/olmoe-pretrain-mose-unbalanced-1012/step30995 \
   --max-sequence-length 2048 \
   --huggingface-output-dir /root/ryanwang/phdbrainstorm/models/olmoe-pretrain-mose-unbalanced-1012/step30995-hf \
   --dtype float32
