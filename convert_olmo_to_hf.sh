133bbebedc1ef4e2ed181f374a6b40cea8996f87
ryanyxw <ryanywan@usc.edu>
2025-10-13 22:23:43 -0700

    added conversion script for olmoe-pretrain-mose-unbalanced-1012

--- convert_olmo_to_hf.sh
+++ convert_olmo_to_hf.sh
@@ -1,7 +1,7 @@
 
 
 python OLMo-core/src/examples/huggingface/convert_checkpoint_to_hf.py \
-  --checkpoint-input-path /root/ryanwang/olmoe-pretrain-replicate/step30995 \
-  --max-sequence-length 4096 \
-  --huggingface-output-dir /root/ryanwang/phdbrainstorm/models/olmoe-pretrain-replicate/step30995-hf \
+  --checkpoint-input-path /root/ryanwang/olmoe-pretrain-mose-unbalanced-1012/step30995 \
+  --max-sequence-length 2048 \
+  --huggingface-output-dir /root/ryanwang/phdbrainstorm/models/olmoe-pretrain-mose-unbalanced-1012/step30995-hf \
   --dtype float32
