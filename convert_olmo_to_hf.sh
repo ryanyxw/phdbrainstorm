15d144304765abe5be2d2847cd5d993d41741de6
ryanyxw <ryanywan@usc.edu>
2025-10-26 10:02:29 -0700

    upload convert

--- convert_olmo_to_hf.sh
+++ convert_olmo_to_hf.sh
@@ -1,7 +1,7 @@
 
 
-python OLMo-core/src/examples/huggingface/convert_checkpoint_to_hf.py \
-  --checkpoint-input-path /root/ryanwang/phdbrainstorm/models/olmoe-pretrain-mose-unbalanced-1012/step30995 \
-  --max-sequence-length 2048 \
-  --huggingface-output-dir /root/ryanwang/phdbrainstorm/models/olmoe-pretrain-mose-unbalanced-1012/step30995-hf \
+python FlexMoE/src/examples/huggingface/convert_checkpoint_to_hf.py \
+  --checkpoint-input-path /root/ryanwang/phdbrainstorm/models/olmo2_1b-pretrain-mose-natural-1025/step30995 \
+  --max-sequence-length 4096 \
+  --huggingface-output-dir /root/ryanwang/phdbrainstorm/models/olmo2_1b-pretrain-mose-natural-1025/step30995-hf \
   --dtype float32
